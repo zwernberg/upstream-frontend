5ec7467f59d56ce4869fa52aa17ee1334446e12a
Zach Wernberg <zawerny@gmail.com>
2015-11-18 20:25:25 -0600

    able to push photos

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+		'ngFileUpload'
   ])
   .config(function ($routeProvider, $httpProvider) {
     $routeProvider
--- app/scripts/controllers/catch.js
+++ app/scripts/controllers/catch.js
@@ -8,8 +8,30 @@
  * Controller of the upstreamApp
  */
 angular.module('upstreamApp')
-  .controller('CatchCtrl', function ($scope, catchService, $resource, $routeParams) {
-	  	
+  .controller('CatchCtrl', function ($scope, catchService, $resource,  Upload, $routeParams, $timeout) {
+    $scope.uploadPic = function(file) {
+    file.upload = Upload.upload({
+      url: 'http://162.243.237.149/api/catches',
+      data: {fishPhoto: file, title: $scope.title},
+    });
+
+    file.upload.then(function (response) {
+      $timeout(function () {
+        file.result = response.data;
+		console.log(response.data);
+		$scope.catches.push(response.data);
+      });
+    }, function (response) {
+      if (response.status > 0)
+        $scope.errorMsg = response.status + ': ' + response.data;
+    }, function (evt) {
+      // Math.min is to fix IE which reports 200% sometimes
+      file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+    });	  	
+	}
+	
+	
+	
 		$scope.catches = catchService.query();		
 		$scope.addCatch = function(addCatch){
 			$scope.newCatch = catchService.save(addCatch,function(){
