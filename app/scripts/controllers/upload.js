30f45d719eecfbf72b6ebee0471bfdbd2d13a247
unknown <benjamin.wernberg@gmail.com>
2015-12-02 21:22:54 -0500

    updated login and upload forms

--- app/scripts/controllers/upload.js
+++ app/scripts/controllers/upload.js
@@ -27,7 +27,7 @@ angular.module('upstreamApp')
     }, function (evt) {
       // Math.min is to fix IE which reports 200% sometimes
       file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-    });	  	
+    });	   	
 	}
 	
 	
