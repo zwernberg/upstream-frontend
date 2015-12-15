c862993701f71625598108d6e7c441201e968a36
unknown <benjamin.wernberg@gmail.com>
2015-12-14 21:05:53 -0500

    testing build

--- app/scripts/controllers/upload.js
+++ app/scripts/controllers/upload.js
@@ -8,7 +8,7 @@
  * Controller of the upstreamApp
  */
 angular.module('upstreamApp')
-  .controller('UploadCtrl', function ($scope, catchService, $resource,  Upload, $routeParams, $timeout) {
+  .controller('UploadCtrl', function ($scope, catchService, $resource,  Upload, $routeParams, $timeout, $location) {
     $scope.uploadPic = function(file) {
     file.upload = Upload.upload({
       url: 'http://162.243.237.149/api/catches',
@@ -16,17 +16,25 @@ angular.module('upstreamApp')
     });
 
     file.upload.then(function (response) {
+     
       $timeout(function () {
         file.result = response.data;
-		console.log(response.data);
+		    console.log(response.data);
+        
       });
     }, function (response) {
-      if (response.status > 0)
+      
+      if (response.status > 0) 
         $scope.errorMsg = response.status + ': ' + response.data;
+        
+        
     }, function (evt) {
       // Math.min is to fix IE which reports 200% sometimes
       file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-    });	   	
+    });
+
+       
+      	
 	}
 	
 	
