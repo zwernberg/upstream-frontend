da966e9655b95396b893766ec3b4eb61ce8913a7
Zach Wernberg <zawerny@gmail.com>
2016-01-06 20:21:25 -0600

    very hacky register

--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -8,10 +8,17 @@
  * Controller of the upstreamApp
  */
 angular.module('upstreamApp')
-  .controller('RegistrationCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RegistrationCtrl', function ($scope, loginService, $http, $window, $location, registerService) {
+		$scope.registerUser = function () {
+			$scope.register = registerService.save({'username': $scope.username,
+			'password1': $scope.password1,'password2': $scope.password2},function(res){
+			}, 
+			function(successResult) {	 
+						$location.path("/");
+			}, function(errorResult) {
+				// do something on error
+				if(errorResult.status === 404) {            
+				}
+			});
+		};
   });
