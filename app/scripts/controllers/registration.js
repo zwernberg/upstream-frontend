3d3168e555a53b97e4f4f67431943b5f07270c31
unknown <benjamin.wernberg@gmail.com>
2016-01-06 22:36:07 -0500

    added client side validation for registration

--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -9,16 +9,18 @@
  */
 angular.module('upstreamApp')
   .controller('RegistrationCtrl', function ($scope, loginService, $http, $window, $location, registerService) {
-		$scope.registerUser = function () {
-			$scope.register = registerService.save({'username': $scope.username,
-			'password1': $scope.password1,'password2': $scope.password2},function(res){
-			}, 
-			function(successResult) {	 
-						$location.path("/");
-			}, function(errorResult) {
-				// do something on error
-				if(errorResult.status === 404) {            
-				}
-			});
-		};
+	  
+			$scope.registerUser = function () {
+				if ($scope.password1.length > 5 && $scope.password1 == $scope.password2)
+					$scope.register = registerService.save({'username': $scope.username,
+					'password1': $scope.password1,'password2': $scope.password2},function(res){
+					}, 
+					function(successResult) {	 
+								$location.path("/");
+					}, function(errorResult) {
+						// do something on error
+						if(errorResult.status === 404) {            
+						}
+					});
+			};
   });
