ba14f12606204ba86b2dc86f5ab5189d03bce826
Zach Wernberg <zawerny@gmail.com>
2016-01-11 22:45:00 -0600

    reauth travis

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 rm -rf out || exit 0;
 git config user.name "Travis-CI"
 git config user.email "travis@upstream.com"
+git remote add upstream "https://$GH_TOKEN@github.com/zwernberg/upstream-frontend.git"
 git add .
 git commit -m "successful travis build $TRAVIS_BUILD_NUMBER"
-git push -fq origin dev > /dev/null
+git push -fq upstream dev > /dev/null
