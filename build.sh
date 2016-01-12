59db32f4137db7b0bc8ebfe43a1aaf08221585e4
Zach Wernberg <zawerny@gmail.com>
2016-01-11 21:22:21 -0600

    update build step to push to master

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ git config user.name "Travis-CI"
 git config user.email "travis@upstream.com"
 git add .
 git commit -m "successful travis build $TRAVIS_BUILD_NUMBER"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
+git push -fq origin master > /dev/null
