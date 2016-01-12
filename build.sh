7a3c1cefc381eeb19eb67b3a0049a3904f192dbe
Zach Wernberg <zawerny@gmail.com>
2016-01-11 23:10:19 -0600

    dist folder

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir out;
 	git clone -b dev "https://${GH_TOKEN}@${GH_REF}" _deploy > /dev/null 2>&1 || exit 1
  git config user.name "Travis-CI"
  git config user.email "travis@nodemeatspace.com"
- cp -r ../dist/* ./dist
+ cp -r ../dist/* dist
  git add .
  git commit -m "Deployed to Github"
 git push origin dev > /dev/null 2>&1 || exit 1
