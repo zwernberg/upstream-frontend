d503360fc4f774284857b272414331bffe21a69f
Zach Wernberg <zawerny@gmail.com>
2016-01-11 22:09:17 -0600

    change branch to dev

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ git config user.name "Travis-CI"
 git config user.email "travis@upstream.com"
 git add .
 git commit -m "successful travis build $TRAVIS_BUILD_NUMBER"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" dev > /dev/null 2>&1
