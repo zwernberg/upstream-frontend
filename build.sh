5d49e305717e46f214830acbe05e7952bd1a713c
Zach Wernberg <zawerny@gmail.com>
2016-01-13 20:25:21 -0600

    bower install and add all

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ rm -rf out || exit 0;
 git clone  "https://${GH_REF}"
 cd upstream-frontend
 git checkout dev
+bower install
 git status
 git config user.name "Travis-CI"
 git config user.email "travis-Builder@upstream.dev"
 grunt build
-git add .
+git add -A
 git commit -m "Deployed to Github"
 git push "https://${GH_TOKEN}@$GITHUB_REPO" dev > /dev/null 2>&1 || exit 1
