10c07e4809cc5873415ef4aa826baf607395eabf
Zach Wernberg <zawerny@gmail.com>
2016-01-13 20:42:08 -0600

    clone correct adress

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 rm -rf out || exit 0;
 
-git clone  "https://${GH_REF}"
+git clone https://${GH_TOKEN}@github.com/zwernberg/upstream-frontend.git
 cd upstream-frontend
 git checkout dev
 bower install
 git status
 git config user.name "Travis-CI"
-git config user.email "travis-Builder@upstream.dev"
+git config user.email "travis-Builder@travis"
 grunt build
 git add -A
 git commit -m "Deployed to Github"
-git push "https://${GH_TOKEN}@{$GITHUB_REPO}" dev > /dev/null 2>&1 || exit 1
+git push origin dev > /dev/null 2>&1 || exit 1
