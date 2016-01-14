6ca5be52635f9130419289aeeb91eec776878c8c
Zach Wernberg <zawerny@gmail.com>
2016-01-13 20:14:04 -0600

    build from shell script

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 rm -rf out || exit 0;
 
-git clone  "https://${GH_REF}" > /dev/null 2>&1 || exit 1
+git clone  "https://${GH_REF}"
 cd upstream-frontend
 git checkout dev
 git status
 git config user.name "Travis-CI"
 git config user.email "travis-Builder@upstream.dev"
-cp -r ../dist/* ./dist
+grunt build
 git add .
 git commit -m "Deployed to Github"
 git push "https://${GH_TOKEN}@$GITHUB_REPO" dev > /dev/null 2>&1 || exit 1
