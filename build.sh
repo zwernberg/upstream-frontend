13c4e98a45f150e47f4a9477da9236173a9d3e91
Zach Wernberg <zawerny@gmail.com>
2016-01-13 19:32:53 -0600

    move into repo folder

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@ rm -rf out || exit 0;
 mkdir out; 
 ( cd out
 	git clone -b dev "https://${GH_TOKEN}@${GH_REF}" _deploy > /dev/null 2>&1 || exit 1
+ cd upstream-frontend
  git config user.name "Travis-CI"
- git config user.email "travis@nodemeatspace.com"
- cp -r ../dist/* upstream-frontend/dist
+ git config user.email "travis-Builder@upstream.dev"
+ cp -r ../dist/* ./dist
  git add .
  git commit -m "Deployed to Github"
 git push origin dev > /dev/null 2>&1 || exit 1
