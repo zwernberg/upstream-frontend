89ae8ee1be8e2c2337b504035644fd5176cadbd4
Zach Wernberg <zawerny@gmail.com>
2016-01-13 19:51:03 -0600

    more build changes

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 rm -rf out || exit 0;
 
 git clone -b dev "https://${GH_TOKEN}@${GH_REF}" _deploy > /dev/null 2>&1 || exit 1
-cd upstream-frontend
-git status
+git remote add github https://${GH_TOKEN}@${GH_REF}
+git remote
 git config user.name "Travis-CI"
 git config user.email "travis-Builder@upstream.dev"
-cp -r ../dist/* ./dist
 git add .
 git commit -m "Deployed to Github"
-git push origin dev > /dev/null 2>&1 || exit 1
+git push github dev > /dev/null 2>&1 || exit 1
