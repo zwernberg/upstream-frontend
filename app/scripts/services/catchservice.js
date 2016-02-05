0f7a2ce7653fd948bdf6de05f6a7badf812ae9aa
Zach Wernberg <zawerny@gmail.com>
2016-02-04 20:09:58 -0600

    fix catches call for pagination

--- app/scripts/services/catchservice.js
+++ app/scripts/services/catchservice.js
@@ -8,7 +8,12 @@
  * Service in the frontendApp.
  */
 angular.module('upstreamApp')
-.factory('catchService', function ($resource) {
+.factory('catchService', function ($resource, $http) {
+		$http.defaults.transformResponse.push(function(data){
+			if(data && data.results){
+				return data.results;
+			}
+		});
       return $resource('http://angler.online/api/catches/:catch',{catch: "@catch"}, {
       update: {
           method:'PUT'
