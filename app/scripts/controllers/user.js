dae9ab5f67aa44bb2855c3911fcf49b1559b3acd
Zach Wernberg <zawerny@gmail.com>
2016-02-29 23:27:38 -0600

    add toast when following/unfollowing

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -8,7 +8,7 @@
  * Controller of the upstreamApp
  */
 angular.module('upstreamApp')
-  .controller('UserCtrl', function ($scope, userService, currentUserService, $resource, $routeParams) {
+  .controller('UserCtrl', function ($scope, userService, currentUserService, $resource, $routeParams, $mdToast) {
 	var user = userService.get({id: $routeParams.userId}, function() {
    		$scope.user = user;
 	});
@@ -17,10 +17,13 @@ angular.module('upstreamApp')
         if (user.following == false) {
             userService.follow({id: currentUser.id});
             user.following = true;
+			$mdToast.show($mdToast.simple().content('You now follow ' + user.username).position('right bottom'));
+
         }
         else {
             userService.unfollow({id: currentUser.id});
             user.following = false;
+			$mdToast.show($mdToast.simple().content('You no longer follow ' + user.username).position('right bottom'));
         }
     }
 		   
