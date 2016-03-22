a4a2c93c1a086caedfb800606becf4d5bfdd1c01
unknown <benjamin.wernberg@gmail.com>
2016-03-21 22:12:22 -0400

    Added placeholder search results

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -9,5 +9,17 @@
  */
 angular.module('upstreamApp')
   .controller('SearchCtrl', function ($scope) {
-
+      $scope.results = [
+          {userId: 3, avatar: 'images/avatar.png', name: 'ben'},
+          {userId: 2, avatar: 'images/avatar.png', name: 'werny'},
+          {userId: 3, avatar: 'images/avatar.png', name: 'sarah'},
+          {userId: 3, avatar: 'images/avatar.png', name: 'mandy'},
+          {userId: 3, avatar: 'images/avatar.png', name: 'matt'},
+          {userId: 3, avatar: 'images/avatar.png', name: 'luke'},
+          {userId: 3, avatar: 'images/avatar.png', name: 'sam'},
+          {userId: 3, avatar: 'images/avatar.png', name: 'samantha'},
+          {userId: 3, avatar: 'images/avatar.png', name: 'samurai'},
+          {userId: 3, avatar: 'images/avatar.png', name: 'huge_fisher'},
+          {userId: 3, avatar: 'images/avatar.png', name: 'salmonhunter'}
+      ]
   });
