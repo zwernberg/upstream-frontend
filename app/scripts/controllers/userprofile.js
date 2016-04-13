c8badf964c0c84c23067accd6a4a28f7ee998315
bwerny <benjamin.wernberg@gmail.com>
2016-04-12 21:52:47 -0400

    Added bio to the controller model

--- app/scripts/controllers/userprofile.js
+++ app/scripts/controllers/userprofile.js
@@ -9,7 +9,7 @@
  */
 angular.module('upstreamApp')
   .controller('UserprofileCtrl', function ($scope, djangoAuth, Validate, $location, $rootScope) {
-    $scope.model = {'first_name':'','last_name':'','email':''};
+    $scope.model = {'first_name':'','last_name':'','email':'', 'bio':''};
   	$scope.complete = false;
   	djangoAuth.profile().then(function(data){
   		$scope.model = data;
